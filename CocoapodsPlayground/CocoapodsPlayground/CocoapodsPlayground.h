7b56f542760bc2ae7d2fc8eb58e0d5fbe1ae095e
Masrina <mas.iosdev@gmail.com>
2021-10-20 17:06:34 +0800

    Initial commit

---
+++ CocoapodsPlayground/CocoapodsPlayground/CocoapodsPlayground.h
@@ -0,0 +1,18 @@
+//
+//  CocoapodsPlayground.h
+//  CocoapodsPlayground
+//
+//  Created by Masrina on 20/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoapodsPlayground.
+FOUNDATION_EXPORT double CocoapodsPlaygroundVersionNumber;
+
+//! Project version string for CocoapodsPlayground.
+FOUNDATION_EXPORT const unsigned char CocoapodsPlaygroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoapodsPlayground/PublicHeader.h>
+
+
---
+++ CocoapodsPlayground/CocoapodsPlayground/CocoapodsPlayground.swift
@@ -0,0 +1,14 @@
+//
+//  CocoapodsPlayground.swift
+//  CocoapodsPlayground
+//
+//  Created by Masrina on 20/10/2021.
+//
+
+import Foundation
+
+class CocoapodsPlayground {
+  public static func greetings(name: String) -> String {
+    return "Hello \(name)"
+  }
+}
